dd7b0af5b1e2ab09258e4d209d006550fd3ba1ef
cosmokhairul <porosh.means.touch@gmail.com>
2022-04-16 14:25:10 +0600

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+require('dotenv').config();
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-//pacificTourism
-//aJDo0lvTXHdrivO2
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bhtwe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 // console.log(uri);
@@ -89,7 +87,7 @@ async function run() {
             const id = req.params.id;
             const updatedStatus = req.body.status;
             const filter = { _id: ObjectId(id) };
-            console.log(updatedStatus);
+            // console.log(updatedStatus);
             ordersCollection
                 .updateOne(filter, {
                     $set: { status: updatedStatus },
